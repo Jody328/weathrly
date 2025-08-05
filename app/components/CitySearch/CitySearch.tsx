f364c9d68e65509d2e6c3ad2e487c9a233f63154
Jody328 <jodyvermeulen007@gmail.com>
2025-08-05 21:41:52 +0200

    Fix search icon visibility in input field

--- app/components/CitySearch/CitySearch.tsx
+++ app/components/CitySearch/CitySearch.tsx
@@ -72,7 +72,7 @@ export const CitySearch = ({
   return (
     <div className="relative w-full max-w-md" ref={rootRef}>
       <div className="relative">
-        <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
+        <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none z-10" />
         <input
           type="text"
           value={inputValue}
